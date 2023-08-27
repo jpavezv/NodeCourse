1f0b4e9b09929b3d522f86008bdf9d75d18ebedf
jpavezv <139666618+jpavezv@users.noreply.github.com>
2023-08-26 21:19:45 -0400

    Asincronico Anidado

--- miFileAsync.js
+++ miFileAsync.js
@@ -1,8 +1,13 @@
 //Using Promise, que es asincrónico.
 const fs = require('fs');
 
-fs.readFile('data.txt','utf-8',(err,data) => {
-    fs.writeFileSync('dataOut2,txt',data);
-    console.log(data);
+fs.readFile('data.txt','utf-8',(err,data1) => {
+    if (err) return console.log('Error:!')
+    fs.readFile(`${data1}.txt`,'utf-8',(err,data2) =>{
+      console.log(data2);
+      fs.writeFile('final.txt',`${data2}\n${data1}\n`, 'utf-8',err=>{
+        console.log('Archivo Actualizado!!');
+      })
+    });
 });
 console.log('Reading File..')

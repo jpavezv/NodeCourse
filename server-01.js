e72732db8f3bc5ae564a32500188c684149af3d7
jpavezv <139666618+jpavezv@users.noreply.github.com>
2023-08-28 23:00:03 -0400

    1 binaries uploaded

--- server-01.js
+++ server-01.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs'); // 1. Require the fs module
 
 const app = express();
 const port = 3000;
 
-const storage = multer.memoryStorage();
+// 2. Use multer.diskStorage() to specify the storage location and filename
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads'); // Save to 'uploads' directory. Make sure this directory exists.
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); // Save with current timestamp as prefix
+    }
+});
+
 const upload = multer({ storage: storage });
 
 app.use(express.static(path.join(__dirname, '.')));
 
 app.post('/upload', upload.single('audio'), (req, res) => {
-    // Here, you can save the audio file to disk, database, etc.
-    console.log('Received audio file');
+    // 3. The file is automatically saved to the specified location by multer
+    console.log('Received audio file:', req.file.filename); // Log the saved filename
     res.sendStatus(200);
 });
 
